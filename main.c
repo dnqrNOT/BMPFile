16db5371e06dfa6c01df9b04cc27f2cd086b01e4
dnqrNOT <83612864+dnqrNOT@users.noreply.github.com>
2021-10-10 15:57:26 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,6 +6,6 @@
 int main(int argc, char const *argv[]) {
   /* code */
   printf("%s\n", "Hello YouTube!");
-  createImage("test.bmp", 1920, 1280);
+  createImage("test.bmp", 1920, 1080);
   return 0;
 }
